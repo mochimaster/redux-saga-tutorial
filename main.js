be5cf75a12e205160eebebb00a3de87f1bdc4fea
Kee Ngiam <kngiam@wiser.com>
2019-11-13 11:47:40 -0800

    redux saga tutorial complete

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-import "babel-polyfill"
+import "babel-polyfill";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 
-import Counter from './Counter'
-import reducer from './reducers'
+const sagaMiddleware = createSagaMiddleware();
+import rootSaga from "./sagas";
 
-const store = createStore(reducer)
+import Counter from "./Counter";
+import reducer from "./reducers";
 
-const action = type => store.dispatch({type})
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
+
+const action = type => store.dispatch({ type });
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
-    document.getElementById('root')
-  )
+      onIncrement={() => action("INCREMENT")}
+      onDecrement={() => action("DECREMENT")}
+      inIncrementSync={() => action("INCREMENT_ASYNC")}
+    />,
+    document.getElementById("root")
+  );
 }
 
-render()
-store.subscribe(render)
+render();
+store.subscribe(render);
